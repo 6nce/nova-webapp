bb185b0b7c9f6392c159e03dd0ee7288c8c88077
6nce <vincentvtruong@gmail.com>
2026-01-13 01:29:10 -0800

    Created test ApplicationsPage content to test input and logic

--- nova-webapp/src/pages/ApplicationsPage.jsx
+++ nova-webapp/src/pages/ApplicationsPage.jsx
@@ -1,22 +1,56 @@
 import {useState} from "react";
 
 export default function ApplicationsPage() {
-    const [applications, setApplications] = useState([
-        {company: "Google", role:"Software Engineer"},
-        {company: "Apple", role:"Software Engineer"}
-    ])
+
+    const [formData, setFormData] = useState({
+        company:"",
+        position:"",
+        salary:"",
+        date:"",
+        matchConfidence:5,
+        desiredLevel:5,
+        coverLetter:false,
+        requirements:"",
+        responsibilities:""
+    });
+
+    const handleChange = (e) => {
+        const field = e.target;
+        const value = (field.type === "checkbox") ? field.checked : field.value;
+
+        setFormData({...formData, [field.name]: value});
+
+    };
 
     return (
         <div>
-            <h2>Applications ({applications.length})</h2>
-            <ul>
-                {applications.map(application => (
-                    <li key={application.company}>
-                        <h3>{application.company}</h3>
-                        <span>{application.role}</span>
-                    </li>
-                ))}
-            </ul>
+            <h3>Application for {formData.company}</h3>
+            <label>
+                Company Name:
+                <input
+                    name="company"
+                    value={formData.company}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>
+                Position Applying for:
+                <input
+                    name="position"
+                    value={formData.position}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>
+                Cover Letter Included?:
+                <input
+                    type="checkbox"
+                    name="coverLetter"
+                    checked={formData.coverLetter}
+                    onChange={handleChange}
+                />
+            </label>
+
         </div>
     )
 }
