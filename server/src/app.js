107b577b9bf172b29190ca3a0a73a0508cbcd730
6nce <vincentvtruong@gmail.com>
2026-01-14 04:09:41 -0800

    style: add global Tailwind form styles with @apply feat: add application form with controlled inputs refactor: implement reusable handleChange for form inputs feat: add application submission with UUID and success view feat: add selectable applications list feat: add applications list view feat: set up Express backend server feat: set up postgres db

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,21 @@
 import express from "express"
+import { pool } from "./db/pool.js";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/health", (req, res) => {
     res.json({ok:true});
+})
+
+app.get ("/db-check", async(req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW() as now");
+        res.json({ok:true, dbTime: result.rows[0].now});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ok:false, error: "DB Connection Failed"});
+    }
 })
 
 export default app;
